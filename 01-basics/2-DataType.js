f4524049e348e374e4bb0a23c2d73d43763ca67d
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-02-08 05:46:09 +0000

    basic datatype

---
+++ 01-basics/2-DataType.js
@@ -0,0 +1,25 @@
+
+let name = "Akash" //string dataType
+let age = 19  //num
+let isLoggedIn = true  //boolean
+
+// Primitive-data types in JS
+// Number => 2 to power53
+// bigInt 
+// string => " "
+// boolean => true/false
+// null => standalne value
+
+let state;
+// undefined => we not given value 
+
+// symbol => unique
+
+
+
+//object 
+
+console.log(typeof age);
+
+console.log(typeof undefined); // output is undefined
+console.log(typeof null); // output is object

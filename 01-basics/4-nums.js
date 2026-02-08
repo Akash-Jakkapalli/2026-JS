cca32de2c7332932bbab2cdd432523de9f40bb51
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-02-08 08:41:19 +0000

    basic numbers commit

---
+++ 01-basics/4-nums.js
@@ -0,0 +1,21 @@
+const num = 100
+const score = new Number(99)
+// console.log(score);
+
+const balance = new Number(100)
+
+console.log(balance.toFixed(2)); // 100.00
+console.log(balance.toString().length); // 
+
+const cost = 33.333
+console.log(cost.toPrecision(3));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+
+
+
+

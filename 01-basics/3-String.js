1b74c2bf10371343dadf0dec65bdcc84802f9a9b
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-02-08 08:15:46 +0000

    basic String

---
+++ 01-basics/3-String.js
@@ -0,0 +1,34 @@
+const name = "Akash"
+let repoCount = 15
+
+// String interpolation
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`) 
+
+
+const gameName = new String('cube')
+// console.log(gameName);
+
+// console.log(typeof gameName); // obj
+// console.log(gameName.toUpperCase());
+
+
+const newString = gameName.slice(0, 2)
+// console.log(newString);
+
+const userName = "   Akash Jakkapalli   ";
+
+// console.log(userName);
+// console.log(userName.trim());
+
+const url = "https://akashjakkapalli.com/Aakash%20Jakkapalli"
+
+// console.log(url.replace('%20', '-'));
+
+let myInfo = 'my name is akash-jakkapalli and my age is 21'
+
+// console.log(myInfo.split('-'))
+
+
+
+
+
